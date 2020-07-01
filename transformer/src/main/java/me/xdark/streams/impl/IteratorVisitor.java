a3d950f8e9f014e7941c0c650c78baad73d39c1a
xDark <19853368+xxDark@users.noreply.github.com>
2020-07-01 23:46:08 +0300

    Why IDEA did not track these?

---
+++ transformer/src/main/java/me/xdark/streams/impl/IteratorVisitor.java
@@ -0,0 +1,15 @@
+package me.xdark.streams.impl;
+
+import me.xdark.streams.InstructionVisitor;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+
+public final class IteratorVisitor implements InstructionVisitor {
+    @Override
+    public void visitInstruction(MethodNode method, AbstractInsnNode insn) {
+        method.instructions.set(insn, new MethodInsnNode(INVOKESTATIC, "me/xdark/streams/StreamSupport", "iterator", "(Ljava/util/Collection;)Ljava/util/Iterator;", false));
+    }
+}
